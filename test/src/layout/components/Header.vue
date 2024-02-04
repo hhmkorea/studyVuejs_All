3ecdbbd879cb58b34a1f23b8ce239ea61b02685f
hhmkorea <yuyee81@gmail.com>
2024-02-04 23:49:04 +0900

    </div> 확인 후 수정. 해결.

--- test/src/layout/components/Header.vue
+++ test/src/layout/components/Header.vue
@@ -1,15 +1,17 @@
 <template>
   <header>
     <div class="topmenu">
-      <div class="contentbox"></div>
-        <div class="logo"></div>
+      <div class="contentbox">
+        <div class="logo">
           <button>
-            <img src="/images/logo.png" width="65px" alt="이지스퍼블리싱 로고" />
+            <img src="/images/logo.png" alt="이지스퍼블리싱 로고" />
           </button>
+        </div>
         <div class="system">
             <button class="login">로그인</button>
             <button class="member">회원가입</button>
         </div>
+      </div>
     </div>
     <nav>
       <div class="contentbox"></div>
