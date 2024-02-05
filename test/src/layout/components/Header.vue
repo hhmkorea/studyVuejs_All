ce20b3577bf3a209df43607d8faac558d95045e9
hhmkorea <yuyee81@gmail.com>
2024-02-05 19:15:20 +0900

    Menu:data,method

--- test/src/layout/components/Header.vue
+++ test/src/layout/components/Header.vue
@@ -17,7 +17,7 @@
       <div class="contentbox">
         <ul>
           <li v-for="(item, index) in menulists" :key="index">
-            <button v-html="item"></button>
+            <button v-html="item.menutext"></button>
           </li>
         </ul>
       </div>
@@ -28,8 +28,22 @@
 export default {
   data() {
     return {
-      menulists: ["도서 소개", "자료실", "동영상 강의", "교재 샘플", "회사 소개"],
-    }
-  }
+      //menulists: ["도서 소개", "자료실", "동영상 강의", "교재 샘플", "회사 소개"],
+      menulists: [
+        { menutext: "도서 소개", link: "/book" },
+        { menutext: "자료실", link: "/reference" },
+        { menutext: "동영상 강의", link: "/movieclass" },
+        { menutext: "교재 샘플", link: "/classsample" },
+        { menutext: "회사 소개", link: "/company" },
+      ],
+    };
+  },
+  methods: {
+    goToPage(target) {
+      if(this.$router.currentRoute.path !== target) {
+        this.$router.push(target);
+      }
+    },
+  },
 };
 </script>
