9d357cdd1e9cb9ef3a60ad67330ed01412f6f4a2
hhmkorea <yuyee81@gmail.com>
2024-02-07 23:22:15 +0900

    Main : 반응형 적용

--- test/src/views/main.vue
+++ test/src/views/main.vue
@@ -12,6 +12,13 @@
           <b-button variant="tap">사진 예술</b-button>
           <b-button variant="tap">데이터 과학</b-button>
         </div>
+        <VueSlickCarousel class="mainslider" v-bind="settings">
+          <div class="rollimg" v-for="(item, index) in Newbooks" :key="index">
+            <span class="imgbox"><img :src="item.imgurl" /></span>
+            <strong v-html="item.name"></strong>
+            <span class="textbox" v-html="item.subdec"></span>
+          </div>
+        </VueSlickCarousel>
       </div>
     </selection>
     <selection class="searchbook">
@@ -25,5 +32,39 @@
     </selection>
   </section>
 </template>
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      Newbooks: [
+        {
+          imgurl: "/images/books_image/book01.jpg",
+          name: "Do it! 웹 사이트 따라 만들기",
+          subdec: "HTML, CSS, 자바스크립트 문법서는 공부했지만, 웹 사이트를...",
+        },
+      ],
+      settings: {
+        slidesToShow: 5, // 화면에 나타날 슬라이드 개수
+        arrows: true,    // 버튼 사용 여부
+        dots: false,     // 하단 버튼 사용 여부
+        infinite: true,  // 슬라이드 반복 여부
+        responsive: [
+            // 반응형 옵션
+          {
+            breakpoint: 767,     // 브라우저 크기가 767 이하일 때
+            settings: {
+              slidesToShow: 4,
+            },
+          },
+          {
+            breakpoint: 414,     // 브라우저 크기가 464 이하일 때
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
