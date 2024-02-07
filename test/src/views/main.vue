4a44ce2232fadde1f74bb45099e00b56e9995946
hhmkorea <yuyee81@gmail.com>
2024-02-07 22:45:37 +0900

    Main : Tap 구현

--- test/src/views/main.vue
+++ test/src/views/main.vue
@@ -1,3 +1,29 @@
 <template>
-  <section class="maincontents">메인 영역</section>
+  <section class="maincontents">
+    <selection class="mainbanner"><img src="/images/mainimg.png" /></selection>
+    <selection class="newbook">
+      <h1 class="mainit">
+        새로 나온 도서<span>분야별로 새로 나온 도서를 만나 보세요.</span>
+      </h1>
+      <div class="tapcontent">
+        <div class="tap">
+          <b-button variant="tap active" >IT 전문서</b-button>
+          <b-button variant="tap">업무 능력, 기타</b-button>
+          <b-button variant="tap">사진 예술</b-button>
+          <b-button variant="tap">데이터 과학</b-button>
+        </div>
+      </div>
+    </selection>
+    <selection class="searchbook">
+      <h1 class="mainit">
+        도서 검색<span>찾고자 하는 도서명을 검색해 주세요.</span>
+      </h1>
+    </selection>
+    <selection class="bannermenu"></selection>
+    <selection class="notice">
+      <h1 class="mainit">공지 사항</h1>
+    </selection>
+  </section>
 </template>
+<script setup>
+</script>
