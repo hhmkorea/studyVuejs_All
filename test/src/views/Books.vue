7f3d05585682c8840f4ad31374cdbc411adfabac
hhmkorea <yuyee81@gmail.com>
2024-02-10 21:53:32 +0900

    SubPage : 도서 소개 탭 콘텐츠

--- test/src/views/Books.vue
+++ test/src/views/Books.vue
@@ -34,6 +34,89 @@
               </li>
             </ul>
           </div>
+          <div class="book-detailinfo">
+            <div class="detailTap">
+              <b-form-radio name="detail-tap"
+                            v-model="tapselect"
+                            value="introduce"
+                            button
+                            button-variant="detailtap">
+                <i class="bi bi-mic" />책 소개
+              </b-form-radio>
+              <b-form-radio name="detail-tap"
+                            v-model="tapselect"
+                            value="review"
+                            button
+                            button-variant="detailtap">
+                <i class="bi bi-chat-left-text" />출판사 리뷰
+              </b-form-radio>
+              <b-form-radio name="detail-tap"
+                            v-model="tapselect"
+                            value="order"
+                            button
+                            button-variant="detailtap">
+                <i class="bi bi-layout-text-sidebar-reverse" />목차
+              </b-form-radio>
+              <b-form-radio name="detail-tap"
+                            v-model="tapselect"
+                            value="write"
+                            button
+                            button-variant="detailtap">
+                <i class="bi bi-journal-text" />저자 소개
+              </b-form-radio>
+              <b-form-radio name="detail-tap"
+                            v-model="tapselect"
+                            value="recommendation"
+                            button
+                            button-variant="detailtap">
+                <i class="bi bi-person-check" />추천의 글
+              </b-form-radio>
+              <b-form-radio name="detail-tap"
+                            v-model="tapselect"
+                            value="reference"
+                            button
+                            button-variant="detailtap">
+                <i class="bi bi-inboxes" />자료실
+              </b-form-radio>
+            </div>
+            <template v-if="tapselect === 'introduce'">
+              <div class="detailTapCon">
+                <h1 class="tapcontit">
+                  <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
+                </h1>
+                <p>
+                  웹 어워드 코리아 대상, 최우수상을 수상한 현직 웹 퍼빌리셔의 작업 방식 그대로<br />
+                  웹 사이트 하나 통째로 만들면 나도 어느새 웹 퍼블리셔!!<br />
+                  이 책 한 권이면 웹 사이트 하나가 뚝딱 완성
+                </p>
+              </div>
+            </template>
+            <template v-if="tapselect === 'review'">
+            <div class="detailTapCon">
+              <p>(출판사 리뷰 - ...)</p>
+            </div>
+            </template>
+            <template v-if="tapselect === 'order'">
+            <div class="detailTapCon">
+              <p>(목차 - ...)</p>
+            </div>
+            </template>
+            <template v-if="tapselect === 'write'">
+            <div class="detailTapCon">
+              <p>(저자 소개 - ...)</p>
+            </div>
+            </template>
+            <template v-if="tapselect === 'recommendation'">
+            <div class="detailTapCon">
+              <p>(추천의 글 - ...)</p>
+            </div>
+            </template>
+            <template v-if="tapselect === 'reference'">
+            <div class="detailTapCon">
+              <p>(자료실 - ...)</p>
+            </div>
+            </template>
+          </div>
         </div>
       </section>
     </div> <!--<slot>으로 대체될 영역-->
@@ -127,7 +210,8 @@ export default {
         {label: "ISBN", content: "979-11-6303-119-2 13000"},
         {label: "정가", content: "16,000원"},
         {label: "상태", content: "정상 판매중"},
-      ]
+      ],
+      tapselect: "introduce",
     };
   },
 };
