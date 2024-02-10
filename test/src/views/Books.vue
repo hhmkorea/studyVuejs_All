42f064a8e0acd373850bf3e009b3ab763caa88ec
hhmkorea <yuyee81@gmail.com>
2024-02-10 22:58:56 +0900

    동적라우팅:도서별 컴포넌트 구성 bookDetail1~10 총 10개의 vue파일 생성.

--- test/src/views/Books.vue
+++ test/src/views/Books.vue
@@ -7,142 +7,40 @@
           <strong v-html="item.name"></strong>
         </div>
       </VueSlickCarousel>
-      <section class="bookdetail">
-        <h1 class="booktitle">
-          Do it! 웹 사이트 따라 만들기
-          <span>HTML, CSS, 자바스크립트, JQuery, Ajax로 웹 퍼블리싱</span>
-        </h1>
-        <div class="book-info">
-          <div class="imgview">
-            <div class="imgbook">
-              <img src="/images/books_image/book01.jpg" alt=""/>
-            </div>
-            <div class="bookbtns">
-              <b-button variant="lightgray">
-                <i class="bi bi-search"/>책 미리보기
-              </b-button>
-              <b-button variant="lightgray">
-                <i class="bi bi-hdd"/>전자책
-              </b-button>
-            </div>
-          </div>
-          <div class="infolist">
-            <ul>
-              <li v-for="(item, index) in bookinfolists" :key="index">
-                <span class="label" v-html="item.label"></span>
-                <span class="infocontent" v-html="item.content"></span>
-              </li>
-            </ul>
-          </div>
-          <div class="book-detailinfo">
-            <div class="detailTap">
-              <b-form-radio name="detail-tap"
-                            v-model="tapselect"
-                            value="introduce"
-                            button
-                            button-variant="detailtap">
-                <i class="bi bi-mic" />책 소개
-              </b-form-radio>
-              <b-form-radio name="detail-tap"
-                            v-model="tapselect"
-                            value="review"
-                            button
-                            button-variant="detailtap">
-                <i class="bi bi-chat-left-text" />출판사 리뷰
-              </b-form-radio>
-              <b-form-radio name="detail-tap"
-                            v-model="tapselect"
-                            value="order"
-                            button
-                            button-variant="detailtap">
-                <i class="bi bi-layout-text-sidebar-reverse" />목차
-              </b-form-radio>
-              <b-form-radio name="detail-tap"
-                            v-model="tapselect"
-                            value="write"
-                            button
-                            button-variant="detailtap">
-                <i class="bi bi-journal-text" />저자 소개
-              </b-form-radio>
-              <b-form-radio name="detail-tap"
-                            v-model="tapselect"
-                            value="recommendation"
-                            button
-                            button-variant="detailtap">
-                <i class="bi bi-person-check" />추천의 글
-              </b-form-radio>
-              <b-form-radio name="detail-tap"
-                            v-model="tapselect"
-                            value="reference"
-                            button
-                            button-variant="detailtap">
-                <i class="bi bi-inboxes" />자료실
-              </b-form-radio>
-            </div>
-            <template v-if="tapselect === 'introduce'">
-              <div class="detailTapCon">
-                <h1 class="tapcontit">
-                  <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
-                </h1>
-                <p>
-                  웹 어워드 코리아 대상, 최우수상을 수상한 현직 웹 퍼빌리셔의 작업 방식 그대로<br />
-                  웹 사이트 하나 통째로 만들면 나도 어느새 웹 퍼블리셔!!<br />
-                  이 책 한 권이면 웹 사이트 하나가 뚝딱 완성
-                </p>
-              </div>
-            </template>
-            <template v-if="tapselect === 'review'">
-            <div class="detailTapCon">
-              <h1 class="tapcontit">
-                <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
-              </h1>
-              <p>(출판사 리뷰 - ...)</p>
-            </div>
-            </template>
-            <template v-if="tapselect === 'order'">
-            <div class="detailTapCon">
-              <h1 class="tapcontit">
-                <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
-              </h1>
-              <p>(목차 - ...)</p>
-            </div>
-            </template>
-            <template v-if="tapselect === 'write'">
-            <div class="detailTapCon">
-              <h1 class="tapcontit">
-                <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
-              </h1>
-              <p>(저자 소개 - ...)</p>
-            </div>
-            </template>
-            <template v-if="tapselect === 'recommendation'">
-            <div class="detailTapCon">
-              <h1 class="tapcontit">
-                <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
-              </h1>
-              <p>(추천의 글 - ...)</p>
-            </div>
-            </template>
-            <template v-if="tapselect === 'reference'">
-            <div class="detailTapCon">
-              <h1 class="tapcontit">
-                <strong>"Do it! 웹 사이트 따라 만들기"</strong>를 소개합니다.
-              </h1>
-              <p>(자료실 - ...)</p>
-            </div>
-            </template>
-          </div>
-        </div>
-      </section>
+      <component :is="componentslist[tabIndex]"
+                 :bookUrl="Newbooks[tabIndex].imgurl"
+                 :bookName="Newbooks[tabIndex].name"
+                 :bookDec="Newbooks[tabIndex].dec" >
+      </component>
     </div> <!--<slot>으로 대체될 영역-->
   </Subpage>
 </template>
 <script>
 import Subpage from "@/layout/components/Subpage.vue";
+import BookDetail1 from "@/views/BookDetail1.vue";
+import BookDetail2 from "@/views/BookDetail2.vue";
+import BookDetail3 from "@/views/BookDetail3.vue";
+import BookDetail4 from "@/views/BookDetail4.vue";
+import BookDetail5 from "@/views/BookDetail5.vue";
+import BookDetail6 from "@/views/BookDetail6.vue";
+import BookDetail7 from "@/views/BookDetail7.vue";
+import BookDetail8 from "@/views/BookDetail8.vue";
+import BookDetail9 from "@/views/BookDetail9.vue";
+import BookDetail10 from "@/views/BookDetail10.vue";
 
 export default {
   components: {
     Subpage,
+    BookDetail1,
+    BookDetail2,
+    BookDetail3,
+    BookDetail4,
+    BookDetail5,
+    BookDetail6,
+    BookDetail7,
+    BookDetail8,
+    BookDetail9,
+    BookDetail10
   },
   data() {
     return {
@@ -218,15 +116,19 @@ export default {
           },
         ],
       },
-      bookinfolists: [
-        {label: "저자", content: "김윤미"},
-        {label: "발행일", content: "2019-11-28"},
-        {label: "사양", content: "312쪽 | 188*257mm"},
-        {label: "ISBN", content: "979-11-6303-119-2 13000"},
-        {label: "정가", content: "16,000원"},
-        {label: "상태", content: "정상 판매중"},
+      componentslist: [
+        BookDetail1,
+        BookDetail2,
+        BookDetail3,
+        BookDetail4,
+        BookDetail5,
+        BookDetail6,
+        BookDetail7,
+        BookDetail8,
+        BookDetail9,
+        BookDetail10
       ],
-      tapselect: "introduce",
+      tabIndex: 0,
     };
   },
 };
