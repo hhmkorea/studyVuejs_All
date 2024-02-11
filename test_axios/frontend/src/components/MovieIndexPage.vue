00dd151d3ed3089ff5c1e61fe4bfcfd9ac1fcc16
hhmkorea <yuyee81@gmail.com>
2024-02-11 19:09:24 +0900

    .

--- test_axios/frontend/src/components/MovieIndexPage.vue
+++ test_axios/frontend/src/components/MovieIndexPage.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="movies">
+  <div class="app">
     <h1>5월 마지막주 영화 예매 순위</h1>
     <ul class="movies">
       <li v-for="movie in movies" :key="movie.id" class="item">
         <span class="rank">{{ movie.id }}</span>
         <router-link :to="{ name: 'show', params: { id: movie.id } }">
-          <img v-bind:src="movie.poster" class="poster"/>
+          <img v-bind:src="movie.poster" class="poster" />
         </router-link>
         <div class="detail">
           <strong class="tit">{{ movie.name }}</strong>
@@ -15,8 +15,7 @@
           <router-link
               :to="{ name: 'show', params: { id: movie.id } }"
               class="link"
-          >자세히보기
-          </router-link
+          >자세히보기</router-link
           >
         </div>
       </li>
@@ -26,16 +25,16 @@
 <script>
 const axios = require('axios');
 export default {
-  created() {
-    // 컴포넌트가 생성될 때, /api/movies에 요청을 보냅니다.          
+  created () {
+    // 컴포넌트가 생성될 때, /api/movies에 요청을 보냅니다.
     axios.get('/api/movies')
         .then((res) => {
           this.movies = res.data;
-          console.log(res.data);
-          //console.log(JSON.stringify(res.data)); // json 형태로 보여줌
+          console.log(res);
         })
         .catch((err) => {
           alert(err);
+          console.log(err);
         });
   },
   data() {
--- test_axios/frontend/src/router/index.js
+++ test_axios/frontend/src/router/index.js
@@ -1,29 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router';
-import TestList from '@/components/TestList.vue';
-import MovieLIst from '@/components/MovieIndexPage.vue';
+import Index from '@/components/MovieIndexPage.vue';
 import Show from '@/components/MovieDetailPage.vue';
-import Weather from '@/components/CurrentWeather.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'testList',
-    component: TestList,
+    name: 'index',
+    component: Index,
   },
   {
-    path: '/movieList',
-    name: 'movieList',
-    component: MovieLIst,
-  },
-  {
-    path: '/movieList/:id',
+    path: '/:id',
     name: 'show',
     component: Show,
-  },
-  {
-    path: '/weather',
-    name: 'weather',
-    component: Weather,
   },
 ];
 
