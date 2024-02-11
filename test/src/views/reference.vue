4f7ece2762918e053d91b51a406ce927dbc65a90
hhmkorea <yuyee81@gmail.com>
2024-02-11 10:16:21 +0900

    SPA:공통컴포넌트

--- test/src/views/reference.vue
+++ test/src/views/reference.vue
@@ -4,39 +4,15 @@
       <div class="subpageguide">
         도서와 관련된 추가 자료를 다운로드 할 수 있습니다.
       </div>
-      <div class="listwrap">
-        <div class="listnum">
-          총 <strong v-html="lists.length">237</strong>건이 등록되어 있습니다.
-        </div>
-        <div class="listbox">
-          <div class="listitem" v-for="(item, index) in lists" :key="index">
-            <div class="listhum"><img :src="item.img" /> </div>
-            <div class="listdetail">
-              <strong v-html="item.title"></strong>
-              <div class="listdate">등록일 : {{ item.date }}</div>
-              <div class="listdec" v-html="item.dec"></div>
-              <div class="itembtns">
-                <b-button variant="listdown" :href="item.link1">
-                  <i class="bi bi-folder-symlink" />소스 파일 내려받기</b-button>
-                <b-button variant="listdown" :href="item.link2">
-                  <i class="bi bi-table" />정오표</b-button>
-              </div>
-              <div class="downguide">
-                ※ 다운로드가 안 될 경우 <br />회사에서 접속한 경우 사내 보안
-                등의 이유로 다운로드가 되지 않을 수 있습니다. <br />
-                고객센터로 연락주시면 별도로 송부하겠습니다.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <listComm :lists="lists" />
     </div>
   </Subpage>
 </template>
 <script>
 import Subpage from "@/layout/components/Subpage.vue";
+import listComm from "@/layout/components/listComm.vue";
 export default {
-  components: { Subpage },
+  components: { Subpage, listComm },
   data() {
     return {
       lists: [
