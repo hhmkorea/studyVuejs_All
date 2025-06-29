0bb3af77b8a125ec777f42dc7c5ce2bc5e539d9d
hhmkorea <hhmkorea@nate.com>
2025-06-29 17:42:21 +0900

    vue3-fullstack : ch05-tailwind

--- vue3-fullstack/ch05_myapp3/src/main.js
+++ vue3-fullstack/ch05_myapp3/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/main.css'; // TailwindCSS
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 createApp(App).mount('#app')
