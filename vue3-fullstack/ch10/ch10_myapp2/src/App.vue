b9360c5976b730ceea0e40d13402c6fddfad1f14
hhmkorea <hhmkorea@nate.com>
2025-07-02 18:30:15 +0900

    ch10:pinia 쇼핑몰, 기본 레이아웃 구현

--- vue3-fullstack/ch10/ch10_myapp2/src/App.vue
+++ vue3-fullstack/ch10/ch10_myapp2/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-
-  <router-view/>
+  <div>
+    <HeaderComp />
+    <section class="section">
+      <div class="container">
+        <router-view />
+      </div>
+    </section>
+    <FooterComp />
+  </div>
 </template>
 
+<script setup>
+import HeaderComp from "@/components/HeaderComp.vue";
+import FooterComp from "@/components/FooterComp.vue";
+</script>
---
+++ vue3-fullstack/ch10/ch10_myapp2/src/components/FooterComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>&copy; 2025 자료실 애플리케이션. 모든 권리 보유.</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #f5f5f5;
+  padding: 1rem 0;
+}
+</style>
