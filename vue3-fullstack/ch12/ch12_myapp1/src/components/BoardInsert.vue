df1ee8b5afcfeecd9fcac3a5e1bb07d297f26117
hhmkorea <hhmkorea@nate.com>
2025-07-06 17:18:46 +0900

    ch12: 공지사항 수정 구현

--- vue3-fullstack/ch12/ch12_myapp1/src/components/BoardInsert.vue
+++ vue3-fullstack/ch12/ch12_myapp1/src/components/BoardInsert.vue
@@ -3,7 +3,7 @@
     <h2>공지사항 작성</h2><hr />
     <form @submit.prevent="handleSubmit">
       <label>제목</label><input type="text" v-model="title" required />
-      <label>내용</label><textarea v-model="content" rows="4" required ></textarea>
+      <label>내용</label><textarea v-model="content" rows="5" required ></textarea>
       <label>작성자</label><input type="text" v-model="author" required />
       <button type="submit">등록</button>
     </form>
