3b313af40962545deb822c8878a357aa7343a639
hhmkorea <hhmkorea@nate.com>
2025-07-08 14:52:38 +0900

    ch12: QnA 상세보기 구현

--- vue3-fullstack/ch12/ch12_myapp1/src/components/qna/QnADetail.vue
+++ vue3-fullstack/ch12/ch12_myapp1/src/components/qna/QnADetail.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>BoardDetail</h1>
+  <div class="grid-container">
+    <div class="card">
+      <div class="card-divider">
+        <h1>{{ questionDetail.title}}</h1>
+      </div>
+      <div class="card-section">
+        <p><strong>작성자:</strong>{{ questionDetail.author }}</p>
+        <p><strong>작성일:</strong>{{ new Date(questionDetail.resdate).toLocaleString() }}</p>
+        <p><strong>조회수:</strong>{{ questionDetail.hits }}</p>
+        <p>{{ questionDetail.content }}</p>
+      </div>
+      <div class="button-group">
+        <button @click="goToEdit" class="button primary">수정</button>
+        <button @click="handleDelete" class="button primary">삭제</button>
+        <button v-if="questionDetail.lev === 1" @click="goToAnswer" class="button secondary">답변</button>
+        <button @click="goToList" class="button">목록</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import {useMainStore} from "@/store/index.js";
+import {useRoute, useRouter} from "vue-router";
+import {onMounted} from "vue";
+
+const mainStore = useMainStore();
+const { fetchQuestionDetail, questionDetail, deleteQuestion } = useMainStore();
+
+const route = useRoute();
+const router = useRouter();
+
+onMounted(() => fetchQuestionDetail(route.params.qno));
+
+const goToEdit = () => {
+  router.push({
+    name: 'QnAEdit',
+    params: { qno: route.params.qno },
+  });
+};
+
+const goToAnswer = () => {
+  router.push({
+    name: 'AnswerInsert',
+    params: { qno: route.params.qno },
+  });
+};
+
+const goToList = () => {
+  router.push({
+    name: 'QnAList',
+  });
+};
+
+const handleDelete = async () => {
+  if (confirm("정말 삭제하시겠습니까?")) {
+    await deleteQuestion(route.params.qno);
+    router.push({ name: 'QnAList' });
+  }
+};
+</script>
